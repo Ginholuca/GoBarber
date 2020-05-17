96534bb8b243ade565c36cea3fe899cc513a9f14
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-05-17 15:06:28 -0300

    TDD

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,42 @@
+import { uuid } from 'uuidv4'
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO'
+
+import Appointment from '../../infra/typeorm/entities/Appointment'
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = []
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    )
+    return findAppointment
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment()
+
+    Object.assign(appointment, { id: uuid(), date, provider_id })
+
+    this.appointments.push(appointment)
+
+    return appointment
+  }
+}
+export default AppointmentsRepository
+
+// Retorno de uma function async sempre será uma proise.
+// Promise - promessa que vai executar alguma coisa.
+
+// Quando se cria metodos dentro de uma classe no TS, é recomendado colocar o retorno do metodo.
+
+/**
+ * Repositorio => O detentor das operaçôes que a gente vai fazer em cima dos dados da aplicação.
+ * Sempre que a gente for armazenar qualquer tipo de dado, os repositórios vão ser utilizados para
+ * conseguir realizar as operações em cima desses dados: Criar, ler, editar, deletar , etc.
+ */
