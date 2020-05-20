2371a9ae80a9bcd43688b865a8bd6b723da53b38
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-05-19 23:50:28 -0300

    provider

--- src/modules/user/services/UpdateUserAvatarService.ts
+++ src/modules/user/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import uploadConfig from '@config/upload'
 import { injectable, inject } from 'tsyringe'
 
 import IUsersRepository from '@modules/user/repositories/IUsersRepository'
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider'
 
 import AppError from '@shared/errors/AppError'
 
@@ -19,6 +20,9 @@ class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -27,18 +31,14 @@ class UpdateUserAvatarService {
     if (!user) {
       throw new AppError('Only authenticated users can change avatar.', 401)
     }
+
     if (user.avatar) {
-      // Deletar avatar anterior
-
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar)
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath)
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath)
-      }
+      await this.storageProvider.deleteFile(user.avatar)
     }
 
-    user.avatar = avatarFilename
+    const filename = await this.storageProvider.saveFile(avatarFilename)
+
+    user.avatar = filename
 
     await this.usersRepository.save(user)
 
---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,9 @@
+import { container } from 'tsyringe'
+
+import IStorageprovider from './StorageProvider/models/IStorageProvider'
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
+
+container.registerSingleton<IStorageprovider>(
+  'StorageProvider',
+  DiskStorageProvider,
+)
