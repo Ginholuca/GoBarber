a5782b8b209f19326141b784f9e01784b5ac9b8a
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-06-16 18:20:58 -0300

    ResetPasswordervice

--- src/modules/user/services/ResetPasswordService.spec.ts
+++ src/modules/user/services/ResetPasswordService.spec.ts
@@ -1,21 +1,25 @@
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 
 import FakeUsersRepository from '@modules/user/repositories/fakes/FakeUsersRepository'
 import FakeUserTokensRepository from '@modules/user/repositories/fakes/FakeUserTokensRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
 import ResetPasswordService from './ResetPasswordService'
 
 let fakeUsersRepository: FakeUsersRepository
 let fakeUserTokensRepository: FakeUserTokensRepository
 let resetPassword: ResetPasswordService
+let fakeHashProvider: FakeHashProvider
 
-describe('SendForgotPasswordEmail', () => {
+describe('ResetPaswordService', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository()
     fakeUserTokensRepository = new FakeUserTokensRepository()
+    fakeHashProvider = new FakeHashProvider()
 
     resetPassword = new ResetPasswordService(
       fakeUsersRepository,
       fakeUserTokensRepository,
+      fakeHashProvider,
     )
   })
 
@@ -28,6 +32,8 @@ describe('SendForgotPasswordEmail', () => {
 
     const { token } = await fakeUserTokensRepository.generate(user.id)
 
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
+
     await resetPassword.execute({
       password: '123123',
       token,
@@ -35,9 +41,16 @@ describe('SendForgotPasswordEmail', () => {
 
     const updatedUser = await fakeUsersRepository.findById(user.id)
 
+    expect(generateHash).toHaveBeenCalledWith('123123')
     expect(updatedUser?.password).toBe('123123')
   })
-})
 
-// Hash
-// 2h expiração
+  it('should not be able to reset the password with non-existing token', async () => {
+    await expect(
+      resetPassword.execute({
+        token: 'non-existing-token',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
