394216bf77aa06bc50bce68fb38af80da95ffb8f
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-06-18 18:51:10 -0300

    EtherealMailProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe'
 import IStorageprovider from './StorageProvider/models/IStorageProvider'
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
 
+import IMailProvider from './MailProvider/models/IMailProvider'
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
+
 container.registerSingleton<IStorageprovider>(
   'StorageProvider',
   DiskStorageProvider,
+)
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 )
